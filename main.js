c7c3281e463d0a611943777d8a3ebf79780d3924
Otman Bouchari <obouchari@gmail.com>
2021-07-29 14:53:15 +0100

    Style landing page to use dark theme

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+import "./public/font/font.css";
 import "./style.css";
