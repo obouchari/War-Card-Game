c7fd4d7ff8439df208f230fd7f7a0cb1be5bcf5c
Philip Browne <pbrowne@gmail.com>
2021-07-29 14:11:05 -0400

    make changes to game ui, start on gameboard layout

--- game/game.js
+++ game/game.js
@@ -1,4 +1,4 @@
-import $ from "jquery";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./game.css";
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './game.css';
