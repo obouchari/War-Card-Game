09a7320db504cdbc546f3516764372ef4bba22e4
Otman Bouchari <obouchari@gmail.com>
2021-07-30 10:54:19 +0100

    Replace default mobile navbar with off-canvas menu

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-import "jquery/dist/jquery";
-import "bootstrap/dist/js/bootstrap.bundle";
+import $ from "jquery";
+import { Offcanvas } from "bootstrap/dist/js/bootstrap.esm";
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./public/font/font.css";
 import "./style.css";
+
+const navBarToggler = $(".navbar-toggler");
+const offCanvasNav = $(".offcanvas");
+const bsOffCanvasNav = new Offcanvas(offCanvasNav);
+
+navBarToggler.on("click", (evt) => bsOffCanvasNav.show());
+offCanvasNav.on("click", ".nav-link", (evt) => bsOffCanvasNav.hide());
